58f6c6c50dfba8eb60d1878f3a016299d4773ab5
RelaxFlxk <116946301+RelaxFlxk@users.noreply.github.com>
2024-07-30 16:28:43 +0700

    Add files via upload

---
+++ src/components/Message/index.js
@@ -0,0 +1,10 @@
+import MessageStart from './MessageStart.vue'
+import MessageByFacebook from './MessageByFacebook.vue'
+import FacebookSubscript from './FacebookSubscript.vue'
+import MessageLogin from './MessageLogin.vue'
+export default {
+  MessageStart,
+  MessageByFacebook,
+  FacebookSubscript,
+  MessageLogin
+}
