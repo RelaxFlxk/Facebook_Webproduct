640924d23276ca0555f35c828ef076aaef39ad10
RelaxFlxk <116946301+RelaxFlxk@users.noreply.github.com>
2024-07-30 16:33:37 +0700

    Add files via upload

---
+++ src/components/Welcome/index.js
@@ -0,0 +1,19 @@
+import Register from './Register'
+import RegisterClose from './RegisterClose'
+import UserSubmit from './UserSubmit'
+import ManageShop from './ManageShop'
+import Home from './Home'
+import EmClose from './EmClose.vue'
+import NavbarRegister from './NavbarRegister.vue'
+import CreateLineOAaccount from './CreateLineOAaccount.vue'
+
+export default {
+  Register,
+  UserSubmit,
+  RegisterClose,
+  ManageShop,
+  Home,
+  EmClose,
+  NavbarRegister,
+  CreateLineOAaccount
+}
