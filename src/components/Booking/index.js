bd4ab1d0b5825d04b0be313ea43d35df76086373
RelaxFlxk <116946301+RelaxFlxk@users.noreply.github.com>
2024-07-30 16:05:21 +0700

    Add files via upload

---
+++ src/components/Booking/index.js
@@ -0,0 +1,71 @@
+import Booking from './Booking'
+import BookingField from './BookingField.vue'
+import BookingForm from './BookingForm.vue'
+import BookingQrcode from './BookingQrcode.vue'
+import Thank from './Thank.vue'
+import UserHistory from './UserHistory.vue'
+import ConditionFlow from './ConditionFlow.vue'
+import BookingAddress from './BookingAddress.vue'
+import CheckBookingLatest from './CheckBookingLatest.vue'
+import CheckBookingNext from './CheckBookingNext.vue'
+import ConfirmUser from './ConfirmUser.vue'
+import PaymentUpload from './PaymentUpload.vue'
+import CalendarBooking from './CalendarBooking.vue'
+import BookingFormEmp from './BookingFormEmp.vue'
+import BookingQrcodeCheckConfirmJob from './BookingQrcodeCheckConfirmJob.vue'
+import Realtimequeue from './Realtimequeue.vue'
+import BookingFormNoLine from './BookingFormNoLine.vue'
+import CalendarBookingEmp from './CalendarBookingEmp.vue'
+import CheckQueueStoreFront from './CheckQueueStoreFront.vue'
+import CheckRealdyQueueStoreFront from './CheckRealdyQueueStoreFront.vue'
+import EmpList from './EmpList.vue'
+import UpdateLineUserID from './UpdateLineUserID.vue'
+import CheckQueueStoreFrontToDayByLineUser from './CheckQueueStoreFrontToDayByLineUser.vue'
+import BookingFormAutoEmp from './BookingFormAutoEmp.vue'
+import CalendarBookingAutoEmp from './CalendarBookingAutoEmp'
+import BookingFormEmpNoLine from './BookingFormEmpNoLine.vue'
+import EditBooking from './EditBooking.vue'
+import CheckQueueStoreFrontOhRich from './CheckQueueStoreFrontOhRich.vue'
+import CheckQueueStoreFrontKioskS from './CheckQueueStoreFrontKioskS.vue'
+import CalendarBookingEmp2 from './CalendarBookingEmp2.vue'
+import CalendarBookingNoLine from './CalendarBookingNoLine.vue'
+import CalendarBookingEmpNoLine from './CalendarBookingEmpNoLine.vue'
+import CalendarBookingAutoEmpNoLine from './CalendarBookingAutoEmpNoLine.vue'
+import BookingFormAutoEmpNoLine from './BookingFormAutoEmpNoLine.vue'
+
+export default {
+  Booking,
+  BookingForm,
+  BookingFormEmp,
+  BookingField,
+  BookingQrcode,
+  Thank,
+  UserHistory,
+  ConditionFlow,
+  BookingAddress,
+  CheckBookingLatest,
+  CheckBookingNext,
+  ConfirmUser,
+  PaymentUpload,
+  CalendarBooking,
+  BookingQrcodeCheckConfirmJob,
+  Realtimequeue,
+  BookingFormNoLine,
+  CalendarBookingEmp,
+  CalendarBookingEmp2,
+  CheckQueueStoreFront,
+  CheckRealdyQueueStoreFront,
+  EmpList,
+  UpdateLineUserID,
+  CheckQueueStoreFrontToDayByLineUser,
+  BookingFormAutoEmp,
+  CalendarBookingAutoEmp,
+  BookingFormEmpNoLine,
+  EditBooking,
+  CheckQueueStoreFrontOhRich,
+  CheckQueueStoreFrontKioskS,
+  CalendarBookingNoLine,
+  CalendarBookingEmpNoLine,
+  CalendarBookingAutoEmpNoLine,
+  BookingFormAutoEmpNoLine
+}
