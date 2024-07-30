19cbb893be12f813f5204a9f772e4d53a9d616c6
RelaxFlxk <116946301+RelaxFlxk@users.noreply.github.com>
2024-07-30 16:32:33 +0700

    Add files via upload

---
+++ src/components/TCG/index.js
@@ -0,0 +1,15 @@
+import Thank from './Thank.vue'
+import UserHistory from './UserHistory.vue'
+import CheckBookingNext from './CheckBookingNext.vue'
+import BookingFormEmp from './BookingFormEmp.vue'
+import CalendarBookingEmp from './CalendarBookingEmp.vue'
+import EmpList from './EmpList.vue'
+
+export default {
+  BookingFormEmp,
+  Thank,
+  UserHistory,
+  CheckBookingNext,
+  CalendarBookingEmp,
+  EmpList
+}
