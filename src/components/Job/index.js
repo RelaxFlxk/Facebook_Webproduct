0f70398e5faa174b56b221e4b86a3464d8ba7434
RelaxFlxk <116946301+RelaxFlxk@users.noreply.github.com>
2024-07-30 16:18:48 +0700

    Add files via upload

---
+++ src/components/Job/index.js
@@ -0,0 +1,13 @@
+
+import jobConfirm from './jobConfirm'
+import rating from './rating.vue'
+import OnsiteRating from './OnsiteRating.vue'
+import stampStep from './stampStep.vue'
+import stampFirstStep from './stampFirstStep.vue'
+export default {
+  jobConfirm,
+  rating,
+  OnsiteRating,
+  stampStep,
+  stampFirstStep
+}
